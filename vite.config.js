f1df3d55bc2a2d0fda56e2df1751d739c6125571
bastianNebra <123bastianlontsi@gmail.com>
2024-09-27 10:48:16 +0200

    update vite config

--- vite.config.js
+++ vite.config.js
@@ -12,8 +12,8 @@ export default defineConfig({
         vue(),
     ],
     resolve:{
-        alias:{
-            vue: "vue/dist/vue.esm-bundler.js"
-        }
+        alias: {
+            '@': '/resources/js',  
+          }
     }
 });
